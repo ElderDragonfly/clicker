a2bdc336100d5d820ea5a2abd62e365cc2f03765
Konstantin <ElderDragonfly@yandex.ru>
2023-03-14 22:40:05 +0300

    работа над таймером

--- script_clicker.js
+++ script_clicker.js
@@ -1,9 +1,12 @@
 "use strict";
 
-const centralFigue = document.querySelector('#central');
-const corners = document.querySelectorAll('.corner');
-const gameOver = document.querySelector('.gameover');
-const main = document.querySelector('.main');
+const centralFigue = document.querySelector('#central'),
+      corners = document.querySelectorAll('.corner'),
+      lose = document.querySelector('.lose'),
+      win = document.querySelector('.win'),
+      gameOver = document.querySelectorAll('.gameover'),
+      main = document.querySelector('.main'),
+      score = document.querySelector('.score');
 let newSquad = document.querySelector('.newSquad');
 
 // рандомайзер
@@ -50,15 +53,6 @@ function getRandomIntExeptCenter(arrW, arrH, width, Height) {
     return [randomNumberW, randomNumberH];
 }
 
-
-// создание и стили хп в центре
-let hitPoint = 100;
-let sizeOfCenterNumber = 50;
-let widthCentralFigue = centralFigue.offsetWidth;
-let heightCentralFigue = centralFigue.offsetHeight;
-centralFigue.innerHTML = `<p class= "centralText">${hitPoint}%</p>`;
-centralFigue.style.cssText = `display: flex; justify-content: center; align-items: center; font-size: ${sizeOfCenterNumber}px; width: 100px; height: 100px`;
-
 // функция на создание случайного нового квадрата
 function createRandomSquare() {
     let randomDiv = document.createElement('div');
@@ -70,45 +64,170 @@ function createRandomSquare() {
     randomDiv.classList.remove('coordinats');
 }
 
-// проверка количества хп для окончания игры
-function checkGameover() {
-    if(hitPoint == 0) {
-        centralFigue.style.fontSize = `0px`;
-        centralFigue.style.width = `0px`;
-        centralFigue.style.height = `0px`;
-
-        gameOver.style.cssText = 'display: flex';
-    } else {
-        centralFigue.style.fontSize = `${sizeOfCenterNumber += 10}px`;
-        centralFigue.style.width = `${widthCentralFigue += 10}px`;
-        centralFigue.style.height = `${heightCentralFigue += 10}px`;
-    }
-    return hitPoint;
-}
-
-// изменение числа хп
+// изменение центрального числа
+let centralNumber = 50;
 function changeHpNumber() {
-        centralFigue.innerHTML = `<p class= "centralText">${hitPoint -= 25}%</p>`;
-        checkGameover();
+    centralFigue.innerHTML = `<p class= "centralText">${centralNumber++}%</p>`;
+    score.innerHTML++;
+    endGame();
 }
+
+// конец игры
+function endGame() {
+    if (centralNumber == 0) {
+        clearInterval(speedOfGame);
+        lose.style.cssText = 'display: flex';
+    }
+    if (centralNumber == 100) {
+        clearInterval(speedOfGame);
+        win.style.cssText = 'display: flex';
+    }
+}
+
+// проверка на количество активных квадратов
+let currentMaxSquad = 1;
+let currentSquadNumber = 0;
+function checkNumbersOfSquad() {
+    if (currentSquadNumber < currentMaxSquad) {
+        createRandomSquare();
+        console.log(currentSquadNumber++);
+    }
+}
+const squadeCreateTimer = setInterval(checkNumbersOfSquad, 10);
+
+// скорость игры
+let speed = 3000;
+function decreaseСentralNumber() {
+    centralFigue.innerHTML = `<p class= "centralText">${centralNumber--}%</p>`;
+    nextLevelStageOne();
+    endGame();
+}
+let speedOfGame = setInterval(decreaseСentralNumber, speed);
+
+// увеличение количества квадратов
+function nextLevelStageOne() {
+    if (centralNumber > 70) {
+        currentMaxSquad = 2;
+        clearInterval(speedOfGame);
+        speed = 2000;
+        speedOfGame = setInterval(decreaseСentralNumber, speed);
+    }
+    if (centralNumber > 85) {
+        currentMaxSquad = 3;
+        clearInterval(speedOfGame);
+        speed = 1000;
+        speedOfGame = setInterval(decreaseСentralNumber, speed);
+    }
+}
+// setInterval(nextLevelStageOne, 10);
 
 // Основное событие
-createRandomSquare();
 main.addEventListener('click', (event) => {
-    if (event.target && event.target.matches('div.newSquad')) {
-        event.target.classList.add('moveToCenter');
-        event.target.classList.remove('newSquad');
+    let target = event.target;
 
-        createRandomSquare();
+    if (target && target.matches('div.newSquad')) {
+        target.classList.add('moveToCenter');
+        target.classList.remove('newSquad');
+
+        currentSquadNumber--;
 
         setTimeout (() => {
             changeHpNumber();
-            event.target.remove();
+            target.remove();
         },2500);
     }
 });
 
+// переключение в блоке статистики
+let tabs = document.querySelectorAll('.stats_item'),
+    tabParent = document.querySelector('.stats');
+
+function hideTabContant() {
+    tabs.forEach(element => {
+        element.classList.add('hide');
+        element.classList.remove('show');
+    });
+}
+
+function showTabContant(i = 0) {
+    tabs[i].classList.remove('hide');
+    tabs[i].classList.add('show');
+}
+
+hideTabContant();
+showTabContant();
+
+let numberOfTabs =0;
+tabParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('rightArrow__img')) {
+        numberOfTabs++;
+        if(numberOfTabs > 2) {
+            numberOfTabs = 0;
+        }
+        hideTabContant();
+        showTabContant(numberOfTabs);
+    }
+
+    if (target && target.classList.contains('leftArrow__img')) {
+        numberOfTabs--;
+        if(numberOfTabs < 0) {
+            numberOfTabs = 2;
+        }
+        hideTabContant();
+        showTabContant(numberOfTabs);
+    }
+});
+
+// таймер
+let seconds = document.querySelector('.seconds'),
+    minutes = document.querySelector('.minutes'),
+    ours = document.querySelector('.ours');
+
+let numberOfSeconds = 0;
+function increaseSeconds() {
+    if (numberOfSeconds > 2) {
+        numberOfSeconds = 0;
+    }
+    if (numberOfSeconds < 10) {
+        seconds.innerHTML = `0${numberOfSeconds++}`;
+        return;
+    }
+    seconds.innerHTML = `${numberOfSeconds++}`;
+}
+let secondsTimer = setInterval(increaseSeconds, 1000);
+
+let numberOfMinutes = 0;
+function increaseMinutes() {
+    if (numberOfMinutes > 59) {
+        numberOfMinutes = 0;
+    }
+    if (numberOfMinutes < 10) {
+        minutes.innerHTML = `0${numberOfMinutes++}`;
+        return;
+    }
+    minutes.innerHTML= `${numberOfMinutes++}`;
+}
+let minutesTimer = setInterval(increaseMinutes, 2000);
+
+let numberOfOurs = 0;
+function increaseOurs() {
+    if (numberOfOurs > 59) {
+        numberOfOurs = 0;
+    }
+    if (numberOfOurs < 10) {
+        ours.innerHTML = `0${numberOfOurs++}`;
+        return;
+    }
+    ours.innerHTML= `${numberOfOurs++}`;
+}
+let oursTimer = setInterval(increaseOurs, 1000*60*60);
+
+
 // перезагрузка страницы после окончания игры
-gameOver.addEventListener('click', () => {
-    window.location.reload();
+gameOver.forEach(element => {
+    element.addEventListener('click', () => {
+        window.location.reload();
+    });
 });
